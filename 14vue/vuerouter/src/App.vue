37e037136aa27c1f2ffdeed4ec9e2fca6ec89172
jiangtao <1433793105@qq.com>
2021-02-07 02:35:43 +0800

    add vuecli

---
+++ 14vue/vuerouter/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-link to="/home" tag="button">首页</router-link>
+    <router-link to="/about" tag="button">关于</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
